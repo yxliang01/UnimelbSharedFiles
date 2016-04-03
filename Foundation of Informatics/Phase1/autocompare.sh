7173de15a86bb219e78c960ab954f683dfe5e9a4
Yxliang <13267.okk@gmail.com>
2016-04-03 20:26:29 +1000

    added auto python detection when there's no runtime argument passed to the bash scripts

--- Foundation of Informatics/Phase1/autocompare.sh
+++ Foundation of Informatics/Phase1/autocompare.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+
 #Author: Xiaoliang Yu
 #This bash script takes one argument which is the path to the python interpreter
 #You must place test_phase1.py in the working directory
 #This script will automatically compare wine.xml, adult.xml and iris.xml with output-wine.xml, output-adult.xml and output-iris.xml correspondingly
 
+if [ -z ${1+x} ]; then
+python="python2"
+else
+python=$1
+fi
+
 for file in adult wine iris
 do
-	$1 ./test_phase1.py "$file".xml output-"$file".xml
+	$python ./test_phase1.py "$file".xml output-"$file".xml
 done
--- Foundation of Informatics/Phase1/localscripttest.sh
+++ Foundation of Informatics/Phase1/localscripttest.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
 #Author: Xiaoliang Yu
 #This bash script takes one argument which is the path to the python interpreter
 #wine.csv, adult.csv and iris.csv must be placed in the working directory
 #wine.xml, adult.xml and iris.xml will be the output files
 
+if [ -z ${1+x} ]; then
+python="python2"
+else
+python=$1
+fi
+
 for file in "wine" "adult" "iris"
 do 
 	cp "$file.csv" "input.csv"
-	$1 phase1.py 
+	$python phase1.py 
 	mv "output.xml" "$file".xml
 	rm "input.csv"
 done 
